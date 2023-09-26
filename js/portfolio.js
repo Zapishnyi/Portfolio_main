d813901cb869e6237bc5ca05071ba20a5503ad91
Zapishnyi <zapishny@ukr.net>
2023-09-26 22:07:54 +0300

    test

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,28 +1,28 @@
 const welcome = document.querySelector(".welcome_page");
-const content_1 = document.querySelector(".projects");
-const content_2 = document.querySelector(".contacts");
-const content_3 = document.querySelector(".about_me");
 const content = document.querySelectorAll(".section");
+const buttons = [
+  document.querySelector(".button_1"),
+  document.querySelector(".button_2"),
+  document.querySelector(".button_3"),
+];
+const content_array = [
+  document.querySelector(".projects"),
+  document.querySelector(".contacts"),
+  document.querySelector(".about_me"),
+];
 
-const button_1 = document.querySelector(".button_1");
-const button_2 = document.querySelector(".button_2");
-const button_3 = document.querySelector(".button_3");
 let content_height = window.innerHeight;
-const buttons = [button_1, button_2, button_3];
-const content_array = [content_1, content_2, content_3];
+
 let button_colors = ["255, 161, 161", "255, 161, 161", "255, 161, 161"];
 
 function section_size() {
   content_height = window.innerHeight;
-
   content.forEach((item) => {
     item.style.cssText = "height :" + (content_height - 80) + "px;";
   });
 }
-section_size();
 
 function button_color_switch() {
-  console.log(button_colors);
   content_array.forEach((element, index) => {
     (element.getBoundingClientRect().y <= 80) &
     (element.getBoundingClientRect().y >= -(content_height - 161))
@@ -33,26 +33,31 @@ function button_color_switch() {
         (buttons[index].style.cssText =
           "background-color : rgb(" + button_colors[index] + ");"));
   });
-  console.log(button_colors);
 }
 
 function button_up(button, color, scroll_psn) {
   button.target.style.cssText =
-    "box-shadow: 2px 2px 8px rgb(100, 100, 100);background-color : rgb(" +
+    "box-shadow: 0px 0px 20px rgb(199, 255, 135);background-color : rgb(" +
     color +
     ");";
   window.scrollTo({ top: scroll_psn, behavior: "smooth" });
 }
+
+section_size();
+
 addEventListener("scroll", () => {
-  console.log("scroll");
   button_color_switch();
 });
+
+addEventListener("resize", () => {
+  section_size();
+});
+
 document.querySelectorAll("li").forEach((element) => {
   element.addEventListener("click", (button) => {
     let index = button.target.className.slice(-1) - 1;
-    console.log(index);
     button.target.style.cssText =
-      "box-shadow: 0px 0px 0px rgb(100, 100, 100);background-color : rgb(" +
+      "box-shadow: 0px 0px 0px rgb(199, 255, 135);background-color : rgb(" +
       button_colors[index] +
       ");";
     setTimeout(
@@ -63,7 +68,4 @@ document.querySelectorAll("li").forEach((element) => {
       (content_height - 80) * (index + 1)
     );
   });
-});
-addEventListener("resize", () => {
-  section_size();
 });
