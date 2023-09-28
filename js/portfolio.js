f2946bc923cd2109da5b8c5dbf46689d546db5c6
Zapishnyi <zapishny@ukr.net>
2023-09-28 20:33:14 +0300

    correction

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,6 @@
 const welcome = document.querySelector(".welcome_page");
 const content = document.querySelectorAll(".section");
+const nav_panel = document.querySelector(".nav_panel");
 const buttons = [
   document.querySelector(".button_1"),
   document.querySelector(".button_2"),
@@ -12,21 +13,23 @@ const content_array = [
 ];
 
 let content_height = window.innerHeight;
-
+let nav_panel_height = nav_panel.offsetHeight;
 let button_colors = ["105, 59, 59", "105, 59, 59", "105, 59, 59"];
 let button_shadow = ["209, 115, 115", "209, 115, 115", "209, 115, 115"];
 
 function section_size() {
   content_height = window.innerHeight;
   content.forEach((item) => {
-    item.style.cssText = "height :" + (content_height - 80) + "px;";
+    item.style.cssText =
+      "height :" + (content_height - nav_panel_height) + "px;";
   });
 }
 
 function button_color_switch() {
   content_array.forEach((element, index) => {
-    (element.getBoundingClientRect().y <= 80) &
-    (element.getBoundingClientRect().y >= -(content_height - 161))
+    (element.getBoundingClientRect().y <= nav_panel_height) &
+    (element.getBoundingClientRect().y >=
+      -(content_height - nav_panel_height * 2 - 1))
       ? ((button_colors[index] = "137, 255, 123"),
         (button_shadow[index] = "199, 255, 135"),
         (buttons[index].style.cssText =
@@ -58,6 +61,7 @@ addEventListener("scroll", () => {
 });
 
 addEventListener("resize", () => {
+  nav_panel_height = nav_panel.offsetHeight;
   section_size();
 });
 
@@ -68,6 +72,10 @@ document.querySelectorAll("li").forEach((element) => {
       "box-shadow: 0px 0px 0px rgb(209, 115, 115);background-color : rgb(" +
       button_colors[index] +
       ");";
-    setTimeout(button_up, 200, (content_height - 80) * (index + 1));
+    setTimeout(
+      button_up,
+      200,
+      (content_height - nav_panel_height) * (index + 1)
+    );
   });
 });
