d2738b4992610290b0b1eeef4f95c169494e3dd3
Zapishnyi <zapishny@ukr.net>
2023-11-28 20:18:41 +0300

    update

--- projects/Perspective/perspective.js
+++ projects/Perspective/perspective.js
@@ -1,9 +1,10 @@
 const wrapper = document.querySelector(".wrapper");
+const base = document.querySelector(".base");
 wrapper.style.cssText = `perspective-origin: 50% ${
   Math.abs(wrapper.getBoundingClientRect().y) + window.innerHeight / 2
 }px;`;
 // wrapper.style.cssText = `height: ${window.innerHeight * 3}px`;
-addEventListener("scroll", (event) => {
+base.addEventListener("scroll", (event) => {
   console.log(Math.abs(wrapper.getBoundingClientRect().y));
   console.log();
   wrapper.style.cssText = `perspective-origin: 50% ${
